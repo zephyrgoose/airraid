da4dbd4b4417b78bcb100fa258233dcdf1208938
Dan Walter <zeph@zeph.tech>
2023-04-16 05:10:11 +1000

    captured_handshakes functionality

--- wifi_scanner.py
+++ wifi_scanner.py
@@ -1,91 +1,117 @@
+import os
+import sys
+import signal
+import subprocess
 from scapy.all import *
 from collections import defaultdict
-import os
-import subprocess
-import sys
 
-def get_wifi_interfaces():
-    """
-    Get a list of available Wi-Fi interfaces.
-    :return: A list of Wi-Fi interface names
-    """
-    interfaces = os.listdir('/sys/class/net')
-    wifi_interfaces = [iface for iface in interfaces if 'wlan' in iface]
-    return wifi_interfaces
+def signal_handler(signal, frame):
+    print("Disabling monitor mode...")
+    os.system(f"sudo airmon-ng stop {mon_iface}")
+    print("Scan complete.")
+    sys.exit(0)
 
-def enable_monitor_mode(interface):
-    """
-    Enable monitor mode on the given Wi-Fi interface.
-    :param interface: The Wi-Fi interface's name
-    :return: The monitor mode interface's name or None if unsuccessful
-    """
-    try:
-        subprocess.run(["sudo", "ifconfig", interface, "down"], check=True)
-        subprocess.run(["sudo", "iwconfig", interface, "mode", "monitor"], check=True)
-        subprocess.run(["sudo", "ifconfig", interface, "up"], check=True)
-        return interface
-    except subprocess.CalledProcessError:
-        return None
-
-def disable_monitor_mode(interface):
-    """
-    Disable monitor mode on the given Wi-Fi interface.
-    :param interface: The Wi-Fi interface's name
-    """
-    subprocess.run(["sudo", "ifconfig", interface, "down"], check=True)
-    subprocess.run(["sudo", "iwconfig", interface, "mode", "managed"], check=True)
-    subprocess.run(["sudo", "ifconfig", interface, "up"], check=True)
-
-def sniff_wifi_packets(packet_callback, interface, timeout=60):
-    """
-    Sniffs for Wi-Fi packets and calls the provided callback function for each packet.
-    :param packet_callback: The callback function to be executed for each packet
-    :param interface: The Wi-Fi interface's name
-    :param timeout: The duration (in seconds) to sniff for packets
-    """
-    sniff(iface=interface, prn=packet_callback, timeout=timeout, store=0)
-
-def handle_packet(packet):
-    """
-    Handles packets captured by the sniffer and prints access point information.
-    :param packet: The packet captured by the sniffer
-    """
+def process_packet(packet):
     if packet.haslayer(Dot11Beacon):
         bssid = packet[Dot11].addr2
         ssid = packet[Dot11Elt].info.decode()
         channel = int(ord(packet[Dot11Elt:3].info))
-        print(f"SSID: {ssid}, BSSID: {bssid}, Channel: {channel}")
 
-def main():
-    wifi_interfaces = get_wifi_interfaces()
+        if bssid not in networks:
+            networks[bssid] = (ssid, channel)
+            print(f"SSID: {ssid}, BSSID: {bssid}, Channel: {channel}")
+
+def select_networks():
+    print("\nAvailable networks:")
+    for i, (bssid, (ssid, channel)) in enumerate(networks.items(), 1):
+        print(f"{i}. SSID: {ssid}, BSSID: {bssid}, Channel: {channel}")
+
+    selected_indices = input("Select networks to target (comma-separated numbers): ").split(',')
+    selected_bssids = [list(networks.keys())[int(index) - 1] for index in selected_indices]
+
+    return selected_bssids
+
+def choose_action(selected_bssids):
+    action_options = [
+        "Crack the password now",
+        "Save network information as a hashcat-friendly text file"
+    ]
+    print("\nAvailable actions:")
+    for i, option in enumerate(action_options, 1):
+        print(f"{i}. {option}")
+
+    action = int(input("Choose an action (enter the number): "))
+    return action
+
+if __name__ == "__main__":
+    networks = {}
+    signal.signal(signal.SIGINT, signal_handler)
+
+    print("Scanning for Wi-Fi interfaces...")
+    interfaces = os.popen("iwconfig 2>/dev/null | grep '^[a-zA-Z]' | awk '{print $1}'").read().splitlines()
+
+    wifi_interfaces = [iface for iface in interfaces if "wlan" in iface or "wlp" in iface]
+
     if not wifi_interfaces:
         print("No Wi-Fi interfaces found.")
         sys.exit(1)
 
-    print("Available Wi-Fi interfaces:")
-    for idx, iface in enumerate(wifi_interfaces):
-        print(f"{idx + 1}. {iface}")
+    if len(wifi_interfaces) == 1:
+        iface = wifi_interfaces[0]
+        print(f"Using Wi-Fi interface: {iface}")
+    else:
+        for i, iface in enumerate(wifi_interfaces, 1):
+            print(f"{i}. {iface}")
+        iface_index = int(input("Select an interface to use (enter the number): ")) - 1
+        iface = wifi_interfaces[iface_index]
 
-    selected_idx = int(input("Select an interface to use (enter the number): ")) - 1
-    if selected_idx < 0 or selected_idx >= len(wifi_interfaces):
-        print("Invalid selection. Exiting.")
+    print("Enabling monitor mode...")
+    os.system(f"sudo airmon-ng start {iface}")
+    mon_iface = os.popen("iwconfig 2>/dev/null | grep '^[a-zA-Z]' | grep 'Mode:Monitor' | awk '{print $1}'").read().strip()
+
+    if not mon_iface:
+        print(f"Checking monitor mode status for interface {iface}...")
+        iwconfig_output = subprocess.check_output(["iwconfig", iface])
+        if b"Mode:Monitor" not in iwconfig_output:
+            os.system(f"sudo ifconfig {iface} down")
+            os.system(f"sudo iwconfig {iface} mode monitor")
+            os.system(f"sudo ifconfig {iface} up")
+
+            iwconfig_output = subprocess.check_output(["iwconfig", iface])
+            if b"Mode:Monitor" not in iwconfig_output:
+                print("Failed to enable monitor mode.")
+                sys.exit(1)
+            else:
+                mon_iface = iface
+        else:
+            mon_iface = iface
+
+    if not mon_iface:
+        print("Monitor mode is not enabled on any interface. Exiting.")
         sys.exit(1)
 
-    selected_iface = wifi_interfaces[selected_idx]
-    print(f"Enabling monitor mode on {selected_iface}...")
-    monitor_iface = enable_monitor_mode(selected_iface)
-    if not monitor_iface:
-        print("Failed to enable monitor mode. Exiting.")
-        sys.exit(1)
 
-    print("Monitor mode enabled. Starting Wi-Fi scan...")
-    try:
-        sniff_wifi_packets(handle_packet, monitor_iface)
-    finally:
-        print("Disabling monitor mode...")
-        disable_monitor_mode(monitor_iface)
-        print("Scan complete.")
+    print(f"Monitor mode enabled on {mon_iface}. Starting Wi-Fi scan...")
+    sniff(iface=mon_iface, prn=process_packet)
 
-if __name__ == "__main__":
-    main()
+    selected_bssids = select_networks()
+    action = choose_action(selected_bssids)
 
+    if action == 1:
+        # Crack the password now
+        pass
+    elif action == 2:
+        # Save network information as a hashcat-friendly text file
+        filename = "wifi_networks.txt"
+        with open(filename, 'w') as f:
+            for bssid in selected_bssids:
+                ssid, channel = networks[bssid]
+                f.write(f"{ssid},{bssid},{channel}\n")
+
+        print(f"Network information saved to {filename}.")
+    else:
+        print("Invalid action. Exiting.")
+
+    print("Disabling monitor mode...")
+    os.system(f"sudo airmon-ng stop {mon_iface}")
+    print("Operation complete.")
